f7562f9d5fb7171192e6b7cfb649e3548148394a
abeer025 <abeeriqbalahmed@gmail.com>
2024-11-06 22:54:42 +0500

    create mogoDB cluster post api

---
+++ my-learning-app/src/app/api/user/route.js
@@ -0,0 +1,13 @@
+import { connectDB } from "@/lib/dbConnect";
+
+export async function POST(request) {
+    await connectDB();
+    const obj = await request.json();
+    console.log("obj=>", obj)
+  return Response.json("User Post Request");
+}
+
+
+export async function GET(request) {
+return Response.json("User GET Request");
+}
