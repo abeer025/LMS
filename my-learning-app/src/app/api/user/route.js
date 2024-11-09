7cdad0d408b90a2a5ec2b432ac59927cc696a9e6
abeer025 <abeeriqbalahmed@gmail.com>
2024-11-09 16:39:12 +0500

    db connected

--- my-learning-app/src/app/api/user/route.js
+++ my-learning-app/src/app/api/user/route.js
@@ -1,9 +1,27 @@
 import { connectDB } from "@/lib/dbConnect";
+import { UserModal } from "@/lib/Modals/UserModal";
+import bcrypt from "bcrypt";
 
 export async function POST(request) {
-    await connectDB();
-    const obj = await request.json();
-    console.log("obj=>", obj)
+  await connectDB();
+  const obj = await request.json();
+  console.log("obj=>", obj);
+
+  //check this user exist
+  const user = await UserModal.findOne({ email: obj.email });
+  if (user)
+    return Response.json(
+      { error: true, msg: "User Already Exist" },
+      { status: 403 }
+    );
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(obj.password, saltRounds);
+  obj.password = hashedPassword;
+
+  console.log(obj.password)
+  console.log(hashedPassword)
+  
   return Response.json("User Post Request");
 }
 
